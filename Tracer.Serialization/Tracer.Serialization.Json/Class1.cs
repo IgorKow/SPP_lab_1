0a2418c53bed3df6890a54544fdd37f22725da33
Igor Kovalchuk <kowal4uck.gosha@yandex.ru>
2022-09-16 00:02:06 +0300

    Добавьте файлы проекта.

---
+++ Tracer.Serialization/Tracer.Serialization.Json/Class1.cs
@@ -0,0 +1,20 @@
+﻿using Tracer.Serialization.Abstraction.Classes;
+using Tracer.Serialization.Abstractions;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Tracer.Serialization.Json
+{
+    public class JsonTraceResultsSerializer : ITraceResultsSerializer
+    {
+        public void Serialize(List<ThreadInfo> threads, Stream to)
+        {
+            using StreamWriter writer = new(to);
+            using JsonTextWriter jsonWriter = new(writer);
+            JsonSerializer ser = new();
+            ser.Serialize(jsonWriter, threads);
+            jsonWriter.Flush();
+        }
+    }
+}
---
+++ Tracer.Serialization/Tracer.Serialization.Yaml/Class1.cs
@@ -0,0 +1,20 @@
+﻿using Tracer.Serialization.Abstraction.Classes;
+using Tracer.Serialization.Abstractions;
+using System.Text;
+using YamlDotNet.Serialization;
+
+namespace Tracer.Serializaton.Yaml
+{
+    public class YamlTraceResultsSerializer : ITraceResultsSerializer
+    {
+        public void Serialize(List<ThreadInfo> threads, Stream to)
+        {
+            using StreamWriter writer = new(to);
+            TextWriter yamlWriter = writer;
+            Serializer ser = new();
+            ser.Serialize(yamlWriter, threads);
+            yamlWriter.Flush();
+        }
+    }
+}
+
